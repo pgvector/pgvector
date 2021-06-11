e5e7a6ec1557076c0afbe967ffda889dfb87249d
Andrew Kane <andrew@ankane.org>
2021-06-11 03:36:06 -0700

    Fixed message [skip ci]

--- sql/vector--0.1.6--0.1.7.sql
+++ sql/vector--0.1.6--0.1.7.sql
@@ -1,5 +1,5 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "ALTER EXTENSION vector UPDATE TO '0.1.6'" to load this file. \quit
+\echo Use "ALTER EXTENSION vector UPDATE TO '0.1.7'" to load this file. \quit
 
 CREATE FUNCTION array_to_vector(numeric[], integer, boolean) RETURNS vector
 	AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
