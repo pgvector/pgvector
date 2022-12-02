bc6b9e4bbad34d5be52d23d5057433b849802427
Andrew Kane <andrew@ankane.org>
2022-12-01 20:45:27 -0800

    Added more tests for text output

--- test/sql/input.sql
+++ test/sql/input.sql
@@ -3,6 +3,8 @@ CREATE EXTENSION IF NOT EXISTS vector;
 
 SELECT '[1,2,3]'::vector;
 SELECT '[-1,2,3]'::vector;
+SELECT '[1.23456]'::vector;
+SELECT '[1.234567]'::vector;
 SELECT '[hello,1]'::vector;
 SELECT '[NaN,1]'::vector;
 SELECT '[Infinity,1]'::vector;
@@ -16,3 +18,6 @@ SELECT '[1,2,3]'::vector(2);
 
 SELECT unnest('{"[1,2,3]", "[4,5,6]"}'::vector[]);
 SELECT '{"[1,2,3]"}'::vector(2)[];
+
+set extra_float_digits = 3;
+SELECT '[1.23456789]'::vector;
