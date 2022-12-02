35d5cb9f06b609a0bc37c99c6c36fd6ef7bf7400
Andrew Kane <andrew@ankane.org>
2022-12-01 21:07:57 -0800

    Fixed CI (behavior of extra_float_digits changed between Postgres 11 and 12)

--- test/sql/input.sql
+++ test/sql/input.sql
@@ -4,7 +4,6 @@ CREATE EXTENSION IF NOT EXISTS vector;
 SELECT '[1,2,3]'::vector;
 SELECT '[-1,2,3]'::vector;
 SELECT '[1.23456]'::vector;
-SELECT '[1.234567]'::vector;
 SELECT '[hello,1]'::vector;
 SELECT '[NaN,1]'::vector;
 SELECT '[Infinity,1]'::vector;
@@ -18,6 +17,3 @@ SELECT '[1,2,3]'::vector(2);
 
 SELECT unnest('{"[1,2,3]", "[4,5,6]"}'::vector[]);
 SELECT '{"[1,2,3]"}'::vector(2)[];
-
-set extra_float_digits = 3;
-SELECT '[1.23456789]'::vector;
