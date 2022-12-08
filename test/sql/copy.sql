da12d9d8b071a033b3ab929825013e554a77c533
Andrew Kane <andrew@ankane.org>
2022-12-07 16:10:35 -0800

    Fixed copy test on Windows [skip ci]

--- test/sql/copy.sql
+++ test/sql/copy.sql
@@ -6,8 +6,8 @@ INSERT INTO t (val) VALUES ('[0,0,0]'), ('[1,2,3]'), ('[1,1,1]'), (NULL);
 
 CREATE TABLE t2 (val vector(3));
 
-\copy t TO '/tmp/data.bin' WITH (FORMAT binary)
-\copy t2 FROM '/tmp/data.bin' WITH (FORMAT binary)
+\copy t TO 'results/data.bin' WITH (FORMAT binary)
+\copy t2 FROM 'results/data.bin' WITH (FORMAT binary)
 
 SELECT * FROM t2 ORDER BY val;
 
