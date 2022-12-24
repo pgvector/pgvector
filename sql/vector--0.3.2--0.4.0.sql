46b8294d5f9e4ba162bda25b0c8bbc34f9db9a62
Andrew Kane <andrew@ankane.org>
2022-12-23 19:16:44 -0800

    Fixed script for Postgres < 13 [skip ci]

--- sql/vector--0.3.2--0.4.0.sql
+++ sql/vector--0.3.2--0.4.0.sql
@@ -1,4 +1,5 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "ALTER EXTENSION vector UPDATE TO '0.4.0'" to load this file. \quit
 
-ALTER TYPE vector SET (STORAGE = extended);
+-- requires Postgres 13+
+-- ALTER TYPE vector SET (STORAGE = extended);
