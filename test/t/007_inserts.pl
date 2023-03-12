9897ba7f643d705c6cf91f0d3020f2a51441bc25
Andrew Kane <andrew@ankane.org>
2023-03-12 13:13:04 -0700

    Fixed CI

--- test/t/007_inserts.pl
+++ test/t/007_inserts.pl
@@ -34,6 +34,10 @@ $node->pgbench(
 
 sub idx_scan
 {
+	# stats do not update instantaneously
+	# https://www.postgresql.org/docs/current/monitoring-stats.html#MONITORING-STATS-VIEWS
+	sleep(1);
+	$node->safe_psql("postgres", "SELECT pg_stat_clear_snapshot();");
 	$node->safe_psql("postgres", "SELECT idx_scan FROM pg_stat_user_indexes WHERE indexrelid = 'tst_v_idx'::regclass;");
 }
 
