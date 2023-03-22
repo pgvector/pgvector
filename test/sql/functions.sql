d3eb56df079d4c45b78a54c70e5b7c81108ac7a4
Andrew Kane <andrew@ankane.org>
2023-03-22 16:35:56 -0700

    Improved tests

--- test/sql/functions.sql
+++ test/sql/functions.sql
@@ -2,15 +2,19 @@ SELECT '[1,2,3]'::vector + '[4,5,6]';
 SELECT '[1,2,3]'::vector - '[4,5,6]';
 
 SELECT vector_dims('[1,2,3]');
-SELECT round(vector_norm('[1,1]')::numeric, 5);
 
-SELECT round(l2_distance('[1,2]', '[0,0]')::numeric, 5);
+SELECT round(vector_norm('[1,1]')::numeric, 5);
+SELECT vector_norm('[3,4]');
+SELECT vector_norm('[0,1]');
+
+SELECT l2_distance('[0,0]', '[3,4]');
+SELECT l2_distance('[0,0]', '[0,1]');
 SELECT l2_distance('[1,2]', '[3]');
 
 SELECT inner_product('[1,2]', '[3,4]');
 SELECT inner_product('[1,2]', '[3]');
 
-SELECT round(cosine_distance('[1,2]', '[2,4]')::numeric, 5);
+SELECT cosine_distance('[1,2]', '[2,4]');
 SELECT cosine_distance('[1,2]', '[0,0]');
 SELECT cosine_distance('[1,1]', '[1,1]');
 SELECT cosine_distance('[1,1]', '[-1,-1]');
