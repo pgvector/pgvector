b8a73557314bab6d531eb579801884cd7186c933
Andrew Kane <andrew@ankane.org>
2023-06-11 19:12:56 -0700

    Improved test [skip ci]

--- test/sql/input.sql
+++ test/sql/input.sql
@@ -13,6 +13,7 @@ SELECT '[1,2,3]9'::vector;
 SELECT '1,2,3'::vector;
 SELECT '[]'::vector;
 SELECT '[1,]'::vector;
+SELECT '[1a]'::vector;
 SELECT '[1,,3]'::vector;
 SELECT '[1, ,3]'::vector;
 SELECT '[1,2,3]'::vector(2);
