8a30cc86aeba1cde7a8b5086fa241c6190d4efb0
ken <ikenchina@gmail.com>
2023-07-29 19:55:21 +0800

    fix bug : vector_cosine_ops

--- sql/vector.sql
+++ sql/vector.sql
@@ -263,7 +263,7 @@ CREATE OPERATOR CLASS vector_ip_ops
 CREATE OPERATOR CLASS vector_cosine_ops
 	FOR TYPE vector USING ivfflat AS
 	OPERATOR 1 <=> (vector, vector) FOR ORDER BY float_ops,
-	FUNCTION 1 vector_negative_inner_product(vector, vector),
+	FUNCTION 1 cosine_distance(vector, vector),
 	FUNCTION 2 vector_norm(vector),
-	FUNCTION 3 vector_spherical_distance(vector, vector),
+	FUNCTION 3 cosine_distance(vector, vector),
 	FUNCTION 4 vector_norm(vector);
