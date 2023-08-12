7db3b08ef4f0776c88101818e8cd2950a1c98fff
Andrew Kane <andrew@ankane.org>
2023-08-11 19:41:55 -0700

    Simplified tests [skip ci]

--- test/sql/hnsw_options.sql
+++ test/sql/hnsw_options.sql
@@ -1,5 +1,3 @@
-SET enable_seqscan = off;
-
 CREATE TABLE t (val vector(3));
 CREATE INDEX ON t USING hnsw (val vector_l2_ops) WITH (m = 1);
 CREATE INDEX ON t USING hnsw (val vector_l2_ops) WITH (m = 101);
--- test/sql/ivfflat_options.sql
+++ test/sql/ivfflat_options.sql
@@ -1,5 +1,3 @@
-SET enable_seqscan = off;
-
 CREATE TABLE t (val vector(3));
 CREATE INDEX ON t USING ivfflat (val vector_l2_ops) WITH (lists = 0);
 CREATE INDEX ON t USING ivfflat (val vector_l2_ops) WITH (lists = 32769);
