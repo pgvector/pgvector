466ec1ac462d7882b7728b67f42d18d17f500220
Andrew Kane <andrew@ankane.org>
2023-10-22 11:15:14 -0700

    Free tuple

--- src/hnswscan.c
+++ src/hnswscan.c
@@ -185,6 +185,9 @@ hnswgettuple(IndexScanDesc scan, ScanDirection dir)
 			Datum		value = PointerGetDatum(hc->element->vec);
 			bool		isnull = false;
 
+			if (scan->xs_itup)
+				pfree(scan->xs_itup);
+
 			scan->xs_itup = index_form_tuple(scan->xs_itupdesc, &value, &isnull);
 			scan->xs_itup->t_tid = *tid;
 		}
@@ -228,6 +231,9 @@ hnswendscan(IndexScanDesc scan)
 	/* Release pin */
 	if (BufferIsValid(so->buf))
 		ReleaseBuffer(so->buf);
+
+	if (scan->xs_itup)
+		pfree(scan->xs_itup);
 
 	MemoryContextDelete(so->tmpCtx);
 
