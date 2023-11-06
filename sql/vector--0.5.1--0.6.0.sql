b17b9c1ca20a677f7f56107ba8116fa5cf9eafc5
Andrew Kane <andrew@ankane.org>
2023-11-05 18:21:09 -0800

    Updated version [skip ci]

--- sql/vector--0.5.1--0.6.0.sql
+++ sql/vector--0.5.1--0.6.0.sql
@@ -1,5 +1,5 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "ALTER EXTENSION vector UPDATE TO '0.5.2'" to load this file. \quit
+\echo Use "ALTER EXTENSION vector UPDATE TO '0.6.0'" to load this file. \quit
 
 CREATE TYPE svector;
 
