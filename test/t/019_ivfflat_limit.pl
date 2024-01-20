bab5daf6df659e22c9d90ac1a245c07110a0d42a
Andrew Kane <andrew@ankane.org>
2024-01-20 15:19:05 -0800

    Improved order [skip ci]

--- test/t/019_ivfflat_limit.pl
+++ test/t/019_ivfflat_limit.pl
@@ -23,16 +23,16 @@ my $explain = $node->safe_psql("postgres", qq(
 ));
 like($explain, qr/Index Scan/);
 
-# Test limit + offset
-$explain = $node->safe_psql("postgres", qq(
-	EXPLAIN ANALYZE SELECT * FROM tst ORDER BY v <-> '[1,2,3]' LIMIT 90 OFFSET 10;
-));
-like($explain, qr/Index Scan/);
-
 # Test limit with probes
 $explain = $node->safe_psql("postgres", qq(
 	SET ivfflat.probes = 2;
 	EXPLAIN ANALYZE SELECT * FROM tst ORDER BY v <-> '[1,2,3]' LIMIT 200;
+));
+like($explain, qr/Index Scan/);
+
+# Test limit + offset
+$explain = $node->safe_psql("postgres", qq(
+	EXPLAIN ANALYZE SELECT * FROM tst ORDER BY v <-> '[1,2,3]' LIMIT 90 OFFSET 10;
 ));
 like($explain, qr/Index Scan/);
 
@@ -42,16 +42,16 @@ $explain = $node->safe_psql("postgres", qq(
 ));
 like($explain, qr/Seq Scan/);
 
-# Test limit + offset > expected tuples
-$explain = $node->safe_psql("postgres", qq(
-	EXPLAIN ANALYZE SELECT * FROM tst ORDER BY v <-> '[1,2,3]' LIMIT 91 OFFSET 10;
-));
-like($explain, qr/Seq Scan/);
-
 # Test limit > expected tuples with probes
 $explain = $node->safe_psql("postgres", qq(
 	SET ivfflat.probes = 2;
 	EXPLAIN ANALYZE SELECT * FROM tst ORDER BY v <-> '[1,2,3]' LIMIT 201;
+));
+like($explain, qr/Seq Scan/);
+
+# Test limit + offset > expected tuples
+$explain = $node->safe_psql("postgres", qq(
+	EXPLAIN ANALYZE SELECT * FROM tst ORDER BY v <-> '[1,2,3]' LIMIT 91 OFFSET 10;
 ));
 like($explain, qr/Seq Scan/);
 
