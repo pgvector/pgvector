57d5d07ef9881bd42ad8ad36b0553a3ca5f47b78
Andrew Kane <andrew@ankane.org>
2024-01-20 16:24:07 -0800

    Test CTE [skip ci]

--- test/t/020_hnsw_limit.pl
+++ test/t/020_hnsw_limit.pl
@@ -23,6 +23,12 @@ my $explain = $node->safe_psql("postgres", qq(
 ));
 like($explain, qr/Index Scan/);
 
+# Test limit with CTE
+$explain = $node->safe_psql("postgres", qq(
+	EXPLAIN ANALYZE WITH cte AS (SELECT * FROM tst ORDER BY v <-> '[1,2,3]' LIMIT 40) SELECT * FROM cte;
+));
+like($explain, qr/Index Scan/);
+
 # Test limit + offset
 $explain = $node->safe_psql("postgres", qq(
 	EXPLAIN ANALYZE SELECT * FROM tst ORDER BY v <-> '[1,2,3]' LIMIT 30 OFFSET 10;
@@ -32,6 +38,12 @@ like($explain, qr/Index Scan/);
 # Test limit > ef_search
 $explain = $node->safe_psql("postgres", qq(
 	EXPLAIN ANALYZE SELECT * FROM tst ORDER BY v <-> '[1,2,3]' LIMIT 41;
+));
+like($explain, qr/Seq Scan/);
+
+# Test limit > ef_search with CTE
+$explain = $node->safe_psql("postgres", qq(
+	EXPLAIN ANALYZE WITH cte AS (SELECT * FROM tst ORDER BY v <-> '[1,2,3]' LIMIT 41) SELECT * FROM cte;
 ));
 like($explain, qr/Seq Scan/);
 
