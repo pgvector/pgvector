76418fc09359b42601b5051d41e31afb762f0484
Andrew Kane <andrew@ankane.org>
2024-01-23 15:13:14 -0800

    Improved test [skip ci]

--- test/sql/functions.sql
+++ test/sql/functions.sql
@@ -37,11 +37,11 @@ SELECT l1_distance('[0,0]', '[0,1]');
 SELECT l1_distance('[1,2]', '[3]');
 SELECT l1_distance('[3e38]', '[-3e38]');
 
-SELECT hamming_distance('\xFF', '\xFF');
-SELECT hamming_distance('\xFF', '\xFE');
-SELECT hamming_distance('\xFF', '\xFC');
-SELECT hamming_distance('\xFF', '\x00');
-SELECT hamming_distance('\xFF', '\x0000');
+SELECT hamming_distance('\xFFFF', '\xFFFF');
+SELECT hamming_distance('\xFFFF', '\xFFFE');
+SELECT hamming_distance('\xFFFF', '\xFFFC');
+SELECT hamming_distance('\xFFFF', '\x0000');
+SELECT hamming_distance('\xFFFF', '\x00');
 
 SELECT avg(v) FROM unnest(ARRAY['[1,2,3]'::vector, '[3,5,7]']) v;
 SELECT avg(v) FROM unnest(ARRAY['[1,2,3]'::vector, '[3,5,7]', NULL]) v;
