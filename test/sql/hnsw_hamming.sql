ec9e13b5fb676c93367bf5be0f7643f17a580b14
Andrew Kane <andrew@ankane.org>
2024-03-25 15:50:38 -0700

    Improved test [skip ci]

--- test/sql/hnsw_hamming.sql
+++ test/sql/hnsw_hamming.sql
@@ -4,9 +4,9 @@ CREATE TABLE t (val bit(3));
 INSERT INTO t (val) VALUES (B'000'), (B'100'), (B'111'), (NULL);
 CREATE INDEX ON t USING hnsw (val bit_hamming_ops);
 
-INSERT INTO t (val) VALUES ('110');
+INSERT INTO t (val) VALUES (B'110');
 
-SELECT * FROM t ORDER BY val <~> '111';
+SELECT * FROM t ORDER BY val <~> B'111';
 SELECT COUNT(*) FROM (SELECT * FROM t ORDER BY val <~> (SELECT NULL::bit)) t2;
 
 DROP TABLE t;
