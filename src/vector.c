21dfed571934d8caf8c2208fe9b11a04a9470f22
Andrew Kane <andrew@ankane.org>
2024-03-25 14:24:19 -0700

    Fixed CI

--- src/vector.c
+++ src/vector.c
@@ -1190,7 +1190,7 @@ hamming_distance(PG_FUNCTION_ARGS)
 
 	CheckBitLengths(VARBITLEN(a), VARBITLEN(b));
 
-	for (int i = 0; i < VARBITBYTES(a); i++)
+	for (uint32 i = 0; i < VARBITBYTES(a); i++)
 		distance += pg_number_of_ones[ax[i] ^ bx[i]];
 
 	/* TODO Decide on return type */
