3acdbf99e8bee6772cface66b404c287a64d19a7
Andrew Kane <andrew@ankane.org>
2024-03-30 09:05:15 -0700

    Added casting to distance functions in tests [skip ci]

--- test/sql/functions.sql
+++ test/sql/functions.sql
@@ -24,29 +24,29 @@ SELECT vector_norm('[3,4]');
 SELECT vector_norm('[0,1]');
 SELECT vector_norm('[3e37,4e37]')::real;
 
-SELECT l2_distance('[0,0]', '[3,4]');
-SELECT l2_distance('[0,0]', '[0,1]');
-SELECT l2_distance('[1,2]', '[3]');
-SELECT l2_distance('[3e38]', '[-3e38]');
+SELECT l2_distance('[0,0]'::vector, '[3,4]');
+SELECT l2_distance('[0,0]'::vector, '[0,1]');
+SELECT l2_distance('[1,2]'::vector, '[3]');
+SELECT l2_distance('[3e38]'::vector, '[-3e38]');
 
-SELECT inner_product('[1,2]', '[3,4]');
-SELECT inner_product('[1,2]', '[3]');
-SELECT inner_product('[3e38]', '[3e38]');
+SELECT inner_product('[1,2]'::vector, '[3,4]');
+SELECT inner_product('[1,2]'::vector, '[3]');
+SELECT inner_product('[3e38]'::vector, '[3e38]');
 
-SELECT cosine_distance('[1,2]', '[2,4]');
-SELECT cosine_distance('[1,2]', '[0,0]');
-SELECT cosine_distance('[1,1]', '[1,1]');
-SELECT cosine_distance('[1,0]', '[0,2]');
-SELECT cosine_distance('[1,1]', '[-1,-1]');
-SELECT cosine_distance('[1,2]', '[3]');
-SELECT cosine_distance('[1,1]', '[1.1,1.1]');
-SELECT cosine_distance('[1,1]', '[-1.1,-1.1]');
-SELECT cosine_distance('[3e38]', '[3e38]');
+SELECT cosine_distance('[1,2]'::vector, '[2,4]');
+SELECT cosine_distance('[1,2]'::vector, '[0,0]');
+SELECT cosine_distance('[1,1]'::vector, '[1,1]');
+SELECT cosine_distance('[1,0]'::vector, '[0,2]');
+SELECT cosine_distance('[1,1]'::vector, '[-1,-1]');
+SELECT cosine_distance('[1,2]'::vector, '[3]');
+SELECT cosine_distance('[1,1]'::vector, '[1.1,1.1]');
+SELECT cosine_distance('[1,1]'::vector, '[-1.1,-1.1]');
+SELECT cosine_distance('[3e38]'::vector, '[3e38]');
 
-SELECT l1_distance('[0,0]', '[3,4]');
-SELECT l1_distance('[0,0]', '[0,1]');
-SELECT l1_distance('[1,2]', '[3]');
-SELECT l1_distance('[3e38]', '[-3e38]');
+SELECT l1_distance('[0,0]'::vector, '[3,4]');
+SELECT l1_distance('[0,0]'::vector, '[0,1]');
+SELECT l1_distance('[1,2]'::vector, '[3]');
+SELECT l1_distance('[3e38]'::vector, '[-3e38]');
 
 SELECT avg(v) FROM unnest(ARRAY['[1,2,3]'::vector, '[3,5,7]']) v;
 SELECT avg(v) FROM unnest(ARRAY['[1,2,3]'::vector, '[3,5,7]', NULL]) v;
