4d041d9282d2b9aefde0e7e69934fd8db59df102
Andrew Kane <andrew@ankane.org>
2024-03-30 10:19:48 -0700

    Fixed CI

--- test/t/020_hnsw_halfvec_build_recall.pl
+++ test/t/020_hnsw_halfvec_build_recall.pl
@@ -90,7 +90,7 @@ for my $i (0 .. $#operators)
 	));
 
 	# Test approximate results
-	my $min = $operator eq "<#>" ? 0.80 : ($operator eq "<=>" ? 0.85 : 0.99);
+	my $min = $operator eq "<#>" ? 0.80 : ($operator eq "<=>" ? 0.80 : 0.99);
 	test_recall($min, $operator);
 
 	$node->safe_psql("postgres", "DROP INDEX idx;");
