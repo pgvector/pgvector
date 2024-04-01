17e0058927d497a9fd71d426e0c5b3ca3e016159
Andrew Kane <andrew@ankane.org>
2024-03-31 22:42:34 -0700

    Improved recall test

--- test/t/020_hnsw_halfvec_build_recall.pl
+++ test/t/020_hnsw_halfvec_build_recall.pl
@@ -94,7 +94,7 @@ for my $i (0 .. $#operators)
 	));
 
 	# Test approximate results
-	my $min = $operator eq "<#>" ? 0.80 : 0.99;
+	my $min = $operator eq "<#>" ? 0.95 : 0.99;
 	test_recall($min, $operator);
 
 	$node->safe_psql("postgres", "DROP INDEX idx;");
