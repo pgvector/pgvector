483e42b9c42ffddcb8dedff12de781ed41ca1240
Andrew Kane <andrew@ankane.org>
2024-04-03 21:15:57 -0700

    Added tests for varbit [skip ci]

--- test/sql/bit_functions.sql
+++ test/sql/bit_functions.sql
@@ -3,6 +3,7 @@ SELECT hamming_distance(B'111', B'110');
 SELECT hamming_distance(B'111', B'100');
 SELECT hamming_distance(B'111', B'000');
 SELECT hamming_distance(B'111', B'00');
+SELECT hamming_distance(B'111', B'000'::varbit(4));
 
 SELECT jaccard_distance(B'1111', B'1111');
 SELECT jaccard_distance(B'1111', B'1110');
@@ -11,3 +12,4 @@ SELECT jaccard_distance(B'1111', B'1000');
 SELECT jaccard_distance(B'1111', B'0000');
 SELECT jaccard_distance(B'1100', B'1000');
 SELECT jaccard_distance(B'1111', B'000');
+SELECT jaccard_distance(B'1111', B'0000'::varbit(5));
