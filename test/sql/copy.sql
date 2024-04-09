3b2ca7df7acf9b4fe0eecb21969a3a4507502a27
Andrew Kane <andrew@ankane.org>
2024-04-09 16:35:06 -0700

    Added sparsevec to copy test [skip ci]

--- test/sql/copy.sql
+++ test/sql/copy.sql
@@ -1,7 +1,7 @@
-CREATE TABLE t (val vector(3), val2 halfvec(3));
-INSERT INTO t (val, val2) VALUES ('[0,0,0]', '[0,0,0]'), ('[1,2,3]', '[1,2,3]'), ('[1,1,1]', '[1,1,1]'), (NULL, NULL);
+CREATE TABLE t (val vector(3), val2 halfvec(3), val3 sparsevec(3));
+INSERT INTO t (val, val2, val3) VALUES ('[0,0,0]', '[0,0,0]', '{}/3'), ('[1,2,3]', '[1,2,3]', '{1:1,2:2,3:3}/3'), ('[1,1,1]', '[1,1,1]', '{1:1,2:1,3:1}/3'), (NULL, NULL, NULL);
 
-CREATE TABLE t2 (val vector(3), val2 halfvec(3));
+CREATE TABLE t2 (val vector(3), val2 halfvec(3), val3 sparsevec(3));
 
 \copy t TO 'results/data.bin' WITH (FORMAT binary)
 \copy t2 FROM 'results/data.bin' WITH (FORMAT binary)
