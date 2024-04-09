f42ccd451dab44d7a22666e28a4c0b6928cac4d7
Andrew Kane <andrew@ankane.org>
2024-04-09 16:29:23 -0700

    Improved sparsevec input tests [skip ci]

--- test/sql/sparsevec_input.sql
+++ test/sql/sparsevec_input.sql
@@ -4,6 +4,17 @@ SELECT '{1:1.5,3:3.5}/5'::sparsevec::vector(5);
 SELECT '{1:1.5,3:3.5}/5'::sparsevec::vector(4);
 SELECT '[0,1.5,0,3.5,0]'::vector::sparsevec;
 
+SELECT ''::sparsevec;
+SELECT '{'::sparsevec;
+SELECT '{}'::sparsevec;
+SELECT '{}/'::sparsevec;
+SELECT '{}/1a'::sparsevec;
+SELECT '{:1}/1'::sparsevec;
+SELECT '{1:}/1'::sparsevec;
+SELECT '{1a:1}/1'::sparsevec;
+SELECT '{1:1a}/1'::sparsevec;
+SELECT '{1:1,}/1'::sparsevec;
+
 SELECT '{1:0,2:1,3:0}/3'::sparsevec;
 
 SELECT '{2:1,1:1}/2'::sparsevec;
