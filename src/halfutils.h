6fa8b40fa79c1abd6e3126ae02dbc838a4e21957
Luca Giacchino <luca.giacchino@intel.com>
2024-04-09 09:08:26 -0700

    Implement AVX512 distance calculations for halfvec

--- src/halfutils.h
+++ src/halfutils.h
@@ -17,6 +17,9 @@ extern float (*HalfvecL1Distance) (int dim, half * ax, half * bx);
 
 void		HalfvecInit(void);
 
+/* Variables */
+extern bool	halfvec_use_fp16_compute;
+
 /*
  * Check if half is NaN
  */
