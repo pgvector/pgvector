9c96164f2c4727b33c3b883931ea954994a2dc88
Andrew Kane <andrew@ankane.org>
2024-04-09 17:05:42 -0700

    Improved cast tests [skip ci]

--- test/sql/cast.sql
+++ test/sql/cast.sql
@@ -3,13 +3,18 @@ SELECT ARRAY[1.0,2.0,3.0]::vector;
 SELECT ARRAY[1,2,3]::float4[]::vector;
 SELECT ARRAY[1,2,3]::float8[]::vector;
 SELECT ARRAY[1,2,3]::numeric[]::vector;
+
+SELECT '[1,2,3]'::vector::real[];
+
+SELECT '{1,2,3}'::real[]::vector;
+SELECT '{1,2,3}'::real[]::vector(3);
+SELECT '{1,2,3}'::real[]::vector(2);
 SELECT '{NULL}'::real[]::vector;
 SELECT '{NaN}'::real[]::vector;
 SELECT '{Infinity}'::real[]::vector;
 SELECT '{-Infinity}'::real[]::vector;
 SELECT '{}'::real[]::vector;
 SELECT '{{1}}'::real[]::vector;
-SELECT '[1,2,3]'::vector::real[];
 
 SELECT '[1,2,3]'::vector::halfvec;
 SELECT '[1,2,3]'::vector::halfvec(3);
@@ -21,14 +26,14 @@ SELECT '[1,2,3]'::halfvec::vector;
 SELECT '[1,2,3]'::halfvec::vector(3);
 SELECT '[1,2,3]'::halfvec::vector(2);
 
+SELECT '[0,1.5,0,3.5,0]'::vector::sparsevec;
+SELECT '[0,1.5,0,3.5,0]'::vector::sparsevec(5);
+SELECT '[0,1.5,0,3.5,0]'::vector::sparsevec(4);
+
 SELECT '{2:1.5,4:3.5}/5'::sparsevec::vector;
 SELECT '{1:1.5,3:3.5}/5'::sparsevec::vector(5);
 SELECT '{1:1.5,3:3.5}/5'::sparsevec::vector(4);
 SELECT '{}/16001'::sparsevec::vector;
-
-SELECT '[0,1.5,0,3.5,0]'::vector::sparsevec;
-SELECT '[0,1.5,0,3.5,0]'::vector::sparsevec(5);
-SELECT '[0,1.5,0,3.5,0]'::vector::sparsevec(4);
 
 SELECT array_agg(n)::vector FROM generate_series(1, 16001) n;
 SELECT array_to_vector(array_agg(n), 16001, false) FROM generate_series(1, 16001) n;
