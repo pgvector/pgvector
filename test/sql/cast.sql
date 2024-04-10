8a29bf4619cb63a88c1c9afd5b1b0e0d2acc6ca7
Andrew Kane <andrew@ankane.org>
2024-04-10 13:42:30 -0700

    Added more cast tests [skip ci]

--- test/sql/cast.sql
+++ test/sql/cast.sql
@@ -26,6 +26,12 @@ SELECT '[1,2,3]'::halfvec::vector;
 SELECT '[1,2,3]'::halfvec::vector(3);
 SELECT '[1,2,3]'::halfvec::vector(2);
 
+SELECT '{1,2,3}'::real[]::halfvec;
+SELECT '{1,2,3}'::real[]::halfvec(3);
+SELECT '{1,2,3}'::real[]::halfvec(2);
+SELECT '{65520,-65520}'::real[]::halfvec;
+SELECT '{1e-8,-1e-8}'::real[]::halfvec;
+
 SELECT '[0,1.5,0,3.5,0]'::vector::sparsevec;
 SELECT '[0,1.5,0,3.5,0]'::vector::sparsevec(5);
 SELECT '[0,1.5,0,3.5,0]'::vector::sparsevec(4);
