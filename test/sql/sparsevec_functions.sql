df56e9f95f5c65debdb2e7fcc7e622218198a3e8
Andrew Kane <andrew@ankane.org>
2024-04-15 00:55:52 -0700

    Improved sparsevec tests [skip ci]

--- test/sql/sparsevec_functions.sql
+++ test/sql/sparsevec_functions.sql
@@ -31,7 +31,8 @@ SELECT l2_distance('{}/2'::sparsevec, '{2:1}/2');
 SELECT '{}/2'::sparsevec <-> '{1:3,2:4}/2';
 
 SELECT inner_product('{1:1,2:2}/2'::sparsevec, '{1:2,2:4}/2');
-SELECT sparsevec_negative_inner_product('{1:1,2:2}/2', '{1:2,2:4}/2');
+SELECT inner_product('{1:1,2:2}/2'::sparsevec, '{1:3}/1');
+SELECT inner_product('{1:3e38}/1'::sparsevec, '{1:3e38}/1');
 
 SELECT cosine_distance('{1:1,2:2}/2'::sparsevec, '{1:2,2:4}/2');
 SELECT cosine_distance('{1:1,2:2}/2'::sparsevec, '{}/2');
