5f739e97e0bf15e1c437d85ec33f66d9b8d0ffe0
Andrew Kane <andrew@ankane.org>
2024-04-15 00:37:38 -0700

    Improved tests for l1_distance [skip ci]

--- test/sql/sparsevec_functions.sql
+++ test/sql/sparsevec_functions.sql
@@ -42,3 +42,5 @@ SELECT l1_distance('{}/2'::sparsevec, '{1:3,2:4}/2');
 SELECT l1_distance('{}/2'::sparsevec, '{2:1}/2');
 SELECT l1_distance('{1:1,2:2}/2'::sparsevec, '{1:3}/1');
 SELECT l1_distance('{1:3e38}/1'::sparsevec, '{1:-3e38}/1');
+SELECT l1_distance('{1:1,3:3,5:5,7:7}/8'::sparsevec, '{2:2,4:4,6:6,8:8}/8');
+SELECT l1_distance('{1:1,3:3,5:5,7:7,9:9}/9'::sparsevec, '{2:2,4:4,6:6,8:8}/9');
