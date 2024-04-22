1804c63e278f16324b22a4955cc5a9e070b2bcee
Andrew Kane <andrew@ankane.org>
2024-04-22 15:53:13 -0700

    Added more tests for vector distance functions [skip ci]

--- test/sql/vector_functions.sql
+++ test/sql/vector_functions.sql
@@ -46,11 +46,13 @@ SELECT l2_distance('[0,0]'::vector, '[3,4]');
 SELECT l2_distance('[0,0]'::vector, '[0,1]');
 SELECT l2_distance('[1,2]'::vector, '[3]');
 SELECT l2_distance('[3e38]'::vector, '[-3e38]');
+SELECT l2_distance('[1,1,1,1,1,1,1,1,1]'::vector, '[1,1,1,1,1,1,1,4,5]');
 SELECT '[0,0]'::vector <-> '[3,4]';
 
 SELECT inner_product('[1,2]'::vector, '[3,4]');
 SELECT inner_product('[1,2]'::vector, '[3]');
 SELECT inner_product('[3e38]'::vector, '[3e38]');
+SELECT inner_product('[1,1,1,1,1,1,1,1,1]'::vector, '[1,2,3,4,5,6,7,8,9]');
 SELECT '[1,2]'::vector <#> '[3,4]';
 
 SELECT cosine_distance('[1,2]'::vector, '[2,4]');
@@ -62,12 +64,17 @@ SELECT cosine_distance('[1,2]'::vector, '[3]');
 SELECT cosine_distance('[1,1]'::vector, '[1.1,1.1]');
 SELECT cosine_distance('[1,1]'::vector, '[-1.1,-1.1]');
 SELECT cosine_distance('[3e38]'::vector, '[3e38]');
+SELECT cosine_distance('[1,2,3,4,5,6,7,8,9]'::vector, '[1,2,3,4,5,6,7,8,9]');
+SELECT cosine_distance('[1,2,3,4,5,6,7,8,9]'::vector, '[-1,-2,-3,-4,-5,-6,-7,-8,-9]');
 SELECT '[1,2]'::vector <=> '[2,4]';
 
 SELECT l1_distance('[0,0]'::vector, '[3,4]');
 SELECT l1_distance('[0,0]'::vector, '[0,1]');
 SELECT l1_distance('[1,2]'::vector, '[3]');
 SELECT l1_distance('[3e38]'::vector, '[-3e38]');
+SELECT l1_distance('[1,2,3,4,5,6,7,8,9]'::vector, '[1,2,3,4,5,6,7,8,9]');
+SELECT l1_distance('[1,2,3,4,5,6,7,8,9]'::vector, '[0,3,2,5,4,7,6,9,8]');
+SELECT '[0,0]'::vector <+> '[3,4]';
 
 SELECT l2_normalize('[3,4]'::vector);
 SELECT l2_normalize('[3,0]'::vector);
