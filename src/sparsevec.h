2d2b27e8d7eaa8809e2e34190c433bb9eb6e1248
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2024-04-23 17:25:20 +0300

    Remove MAXALIGN from SparseVector
    A SparseVector as whole is only 4-byte aligned, so it's a bit weird to
    add padding to 8 bytes (on 64-bit platforms) in the middle of the
    struct. It doesn't guarantee that that the values would land on an
    8-byte boundary anyway.
    
    For clarity, also turn SPARSEVEC_SIZE and SPARSEVEC_VALUES into static
    inline functions. It's more tidy to add comments on proper functions.
    Also eliminates the double-evaluation hazard of the _nnz argument.

--- src/sparsevec.h
+++ src/sparsevec.h
@@ -6,21 +6,49 @@
 #define SPARSEVEC_MAX_DIM 100000
 #define SPARSEVEC_MAX_NNZ 16000
 
-/* Ensure values are aligned */
-#define SPARSEVEC_SIZE(_nnz)		(offsetof(SparseVector, indices) + MAXALIGN((_nnz) * sizeof(int32)) + (_nnz * sizeof(float)))
-#define SPARSEVEC_VALUES(x)		((float *) (((char *) (x)) + offsetof(SparseVector, indices) + MAXALIGN((x)->nnz * sizeof(int32))))
-#define DatumGetSparseVector(x)		((SparseVector *) PG_DETOAST_DATUM(x))
-#define PG_GETARG_SPARSEVEC_P(x)	DatumGetSparseVector(PG_GETARG_DATUM(x))
-#define PG_RETURN_SPARSEVEC_P(x)	PG_RETURN_POINTER(x)
-
 typedef struct SparseVector
 {
 	int32		vl_len_;		/* varlena header (do not touch directly!) */
 	int32		dim;			/* number of dimensions */
-	int32		nnz;
+	int32		nnz;			/* number of non-zero dimensions */
 	int32		unused;			/* reserved for future use, always zero */
+
+	/*
+	 * indices contains nnz entries, one for each non-zero dimension. Each
+	 * element is the dimension stored a that index. Sorted by dimension.
+	 */
 	int32		indices[FLEXIBLE_ARRAY_MEMBER];
+
+	/*
+	 * The values are stored after the indices:
+	 *
+	 * float4   values[FLEXIBLE_ARRAY_MEMBER];
+	 */
 }			SparseVector;
+
+/* accessor for the 'values' field that is logically part of the struct */
+static inline float *
+SPARSEVEC_VALUES(SparseVector *x)
+{
+	return (float *) &x->indices[x->nnz];
+}
+
+/* size in bytes of a SparseVector */
+static inline size_t
+SPARSEVEC_SIZE(int32 nnz)
+{
+	/*
+	 * Note: No padding is needed, because SparseVector only contains 'int32'
+	 * and 'float4' fields, and the have the same alignment.
+	 */
+	return offsetof(SparseVector, indices) +
+		nnz * sizeof(int32) +		/* indices array */
+		nnz * sizeof(float);		/* values array */
+}
+
+#define DatumGetSparseVector(x)		((SparseVector *) PG_DETOAST_DATUM(x))
+#define PG_GETARG_SPARSEVEC_P(x)	DatumGetSparseVector(PG_GETARG_DATUM(x))
+#define PG_RETURN_SPARSEVEC_P(x)	PG_RETURN_POINTER(x)
 
 SparseVector *InitSparseVector(int dim, int nnz);
 
