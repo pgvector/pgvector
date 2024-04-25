80d34830f6ebc83bb94b6d5637f8a2661f0dd6a3
Andrew Kane <andrew@ankane.org>
2024-04-25 15:35:36 -0700

    Condensed regression tests [skip ci]

--- test/sql/sparsevec.sql
+++ test/sql/sparsevec.sql
@@ -1,396 +1,134 @@
+SELECT '{1:1.5,3:3.5}/5'::sparsevec;
+SELECT '{1:-2,3:-4}/5'::sparsevec;
+SELECT '{1:2.,3:4.}/5'::sparsevec;
+SELECT ' { 1 : 1.5 ,  3  :  3.5  } / 5 '::sparsevec;
+SELECT '{1:1.23456}/1'::sparsevec;
+SELECT '{1:hello,2:1}/2'::sparsevec;
+SELECT '{1:NaN,2:1}/2'::sparsevec;
+SELECT '{1:Infinity,2:1}/2'::sparsevec;
+SELECT '{1:-Infinity,2:1}/2'::sparsevec;
+SELECT '{1:1.5e38,2:-1.5e38}/2'::sparsevec;
+SELECT '{1:1.5e+38,2:-1.5e+38}/2'::sparsevec;
+SELECT '{1:1.5e-38,2:-1.5e-38}/2'::sparsevec;
+SELECT '{1:4e38,2:1}/2'::sparsevec;
+SELECT '{1:-4e38,2:1}/2'::sparsevec;
+SELECT '{1:1e-46,2:1}/2'::sparsevec;
+SELECT '{1:-1e-46,2:1}/2'::sparsevec;
+SELECT ''::sparsevec;
+SELECT '{'::sparsevec;
+SELECT '{ '::sparsevec;
+SELECT '{:'::sparsevec;
+SELECT '{,'::sparsevec;
+SELECT '{}'::sparsevec;
+SELECT '{}/'::sparsevec;
+SELECT '{}/1'::sparsevec;
+SELECT '{}/1a'::sparsevec;
+SELECT '{ }/1'::sparsevec;
+SELECT '{:}/1'::sparsevec;
+SELECT '{,}/1'::sparsevec;
+SELECT '{1,}/1'::sparsevec;
+SELECT '{:1}/1'::sparsevec;
+SELECT '{1:}/1'::sparsevec;
+SELECT '{1a:1}/1'::sparsevec;
+SELECT '{1:1a}/1'::sparsevec;
+SELECT '{1:1,}/1'::sparsevec;
+SELECT '{1:0,2:1,3:0}/3'::sparsevec;
+SELECT '{2:1,1:1}/2'::sparsevec;
+SELECT '{1:1,1:1}/2'::sparsevec;
+SELECT '{1:1,2:1,1:1}/2'::sparsevec;
+SELECT '{}/5'::sparsevec;
+SELECT '{}/-1'::sparsevec;
+SELECT '{}/1000000001'::sparsevec;
+SELECT '{}/2147483648'::sparsevec;
+SELECT '{}/-2147483649'::sparsevec;
+SELECT '{}/9223372036854775808'::sparsevec;
+SELECT '{}/-9223372036854775809'::sparsevec;
+SELECT '{2147483647:1}/1'::sparsevec;
+SELECT '{2147483648:1}/1'::sparsevec;
+SELECT '{-2147483648:1}/1'::sparsevec;
+SELECT '{-2147483649:1}/1'::sparsevec;
+SELECT '{0:1}/1'::sparsevec;
+SELECT '{2:1}/1'::sparsevec;
+
+SELECT '{}/3'::sparsevec(3);
+SELECT '{}/3'::sparsevec(2);
+SELECT '{}/3'::sparsevec(3, 2);
+SELECT '{}/3'::sparsevec('a');
+SELECT '{}/3'::sparsevec(0);
+SELECT '{}/3'::sparsevec(1000000001);
+
 SELECT '{1:1,2:2,3:3}/3'::sparsevec < '{1:1,2:2,3:3}/3';
- ?column? 
-----------
- f
-(1 row)
-
 SELECT '{1:1,2:2,3:3}/3'::sparsevec < '{1:1,2:2}/2';
- ?column? 
-----------
- f
-(1 row)
-
 SELECT '{1:1,2:2,3:3}/3'::sparsevec <= '{1:1,2:2,3:3}/3';
- ?column? 
-----------
- t
-(1 row)
-
 SELECT '{1:1,2:2,3:3}/3'::sparsevec <= '{1:1,2:2}/2';
- ?column? 
-----------
- f
-(1 row)
-
 SELECT '{1:1,2:2,3:3}/3'::sparsevec = '{1:1,2:2,3:3}/3';
- ?column? 
-----------
- t
-(1 row)
-
 SELECT '{1:1,2:2,3:3}/3'::sparsevec = '{1:1,2:2}/2';
- ?column? 
-----------
- f
-(1 row)
-
 SELECT '{1:1,2:2,3:3}/3'::sparsevec != '{1:1,2:2,3:3}/3';
- ?column? 
-----------
- f
-(1 row)
-
 SELECT '{1:1,2:2,3:3}/3'::sparsevec != '{1:1,2:2}/2';
- ?column? 
-----------
- t
-(1 row)
-
 SELECT '{1:1,2:2,3:3}/3'::sparsevec >= '{1:1,2:2,3:3}/3';
- ?column? 
-----------
- t
-(1 row)
-
 SELECT '{1:1,2:2,3:3}/3'::sparsevec >= '{1:1,2:2}/2';
- ?column? 
-----------
- t
-(1 row)
-
 SELECT '{1:1,2:2,3:3}/3'::sparsevec > '{1:1,2:2,3:3}/3';
- ?column? 
-----------
- f
-(1 row)
-
 SELECT '{1:1,2:2,3:3}/3'::sparsevec > '{1:1,2:2}/2';
- ?column? 
-----------
- t
-(1 row)
 
 SELECT sparsevec_cmp('{1:1,2:2,3:3}/3', '{1:1,2:2,3:3}/3');
- sparsevec_cmp 
----------------
-             0
-(1 row)
-
 SELECT sparsevec_cmp('{1:1,2:2,3:3}/3', '{}/3');
- sparsevec_cmp 
----------------
-             1
-(1 row)
-
 SELECT sparsevec_cmp('{}/3', '{1:1,2:2,3:3}/3');
- sparsevec_cmp 
----------------
-            -1
-(1 row)
-
 SELECT sparsevec_cmp('{1:1,2:2}/2', '{1:1,2:2,3:3}/3');
- sparsevec_cmp 
----------------
-            -1
-(1 row)
-
 SELECT sparsevec_cmp('{1:1,2:2,3:3}/3', '{1:1,2:2}/2');
- sparsevec_cmp 
----------------
-             1
-(1 row)
-
 SELECT sparsevec_cmp('{1:1,2:2}/2', '{1:2,2:3,3:4}/3');
- sparsevec_cmp 
----------------
-            -1
-(1 row)
-
 SELECT sparsevec_cmp('{1:2,2:3}/2', '{1:1,2:2,3:3}/3');
- sparsevec_cmp 
----------------
-             1
-(1 row)
 
 SELECT round(l2_norm('{1:1,2:1}/2'::sparsevec)::numeric, 5);
-  round  
----------
- 1.41421
-(1 row)
-
 SELECT l2_norm('{1:3,2:4}/2'::sparsevec);
- l2_norm 
----------
-       5
-(1 row)
-
 SELECT l2_norm('{2:1}/2'::sparsevec);
- l2_norm 
----------
-       1
-(1 row)
-
 SELECT l2_norm('{1:3e37,2:4e37}/2'::sparsevec)::real;
- l2_norm 
----------
-   5e+37
-(1 row)
-
 SELECT l2_norm('{}/2'::sparsevec);
- l2_norm 
----------
-       0
-(1 row)
-
 SELECT l2_norm('{1:2}/1'::sparsevec);
- l2_norm 
----------
-       2
-(1 row)
 
 SELECT l2_distance('{}/2'::sparsevec, '{1:3,2:4}/2');
- l2_distance 
--------------
-           5
-(1 row)
-
 SELECT l2_distance('{1:3}/2'::sparsevec, '{2:4}/2');
- l2_distance 
--------------
-           5
-(1 row)
-
 SELECT l2_distance('{2:4}/2'::sparsevec, '{1:3}/2');
- l2_distance 
--------------
-           5
-(1 row)
-
 SELECT l2_distance('{1:3,2:4}/2'::sparsevec, '{}/2');
- l2_distance 
--------------
-           5
-(1 row)
-
 SELECT l2_distance('{}/2'::sparsevec, '{2:1}/2');
- l2_distance 
--------------
-           1
-(1 row)
-
 SELECT '{}/2'::sparsevec <-> '{1:3,2:4}/2';
- ?column? 
-----------
-        5
-(1 row)
 
 SELECT inner_product('{1:1,2:2}/2'::sparsevec, '{1:2,2:4}/2');
- inner_product 
----------------
-            10
-(1 row)
-
 SELECT inner_product('{1:1,2:2}/2'::sparsevec, '{1:3}/1');
-ERROR:  different sparsevec dimensions 2 and 1
 SELECT inner_product('{1:1,3:3}/4'::sparsevec, '{2:2,4:4}/4');
- inner_product 
----------------
-             0
-(1 row)
-
 SELECT inner_product('{2:2,4:4}/4'::sparsevec, '{1:1,3:3}/4');
- inner_product 
----------------
-             0
-(1 row)
-
 SELECT inner_product('{1:1,3:3,5:5}/5'::sparsevec, '{2:4,3:6,4:8}/5');
- inner_product 
----------------
-            18
-(1 row)
-
 SELECT inner_product('{1:1}/2'::sparsevec, '{}/2');
- inner_product 
----------------
-             0
-(1 row)
-
 SELECT inner_product('{}/2'::sparsevec, '{1:1}/2');
- inner_product 
----------------
-             0
-(1 row)
-
 SELECT inner_product('{1:3e38}/1'::sparsevec, '{1:3e38}/1');
- inner_product 
----------------
-      Infinity
-(1 row)
-
 SELECT inner_product('{1:1,3:3,5:5}/5'::sparsevec, '{2:4,3:6,4:8}/5');
- inner_product 
----------------
-            18
-(1 row)
-
 SELECT '{1:1,2:2}/2'::sparsevec <#> '{1:3,2:4}/2';
- ?column? 
-----------
-      -11
-(1 row)
 
 SELECT cosine_distance('{1:1,2:2}/2'::sparsevec, '{1:2,2:4}/2');
- cosine_distance 
------------------
-               0
-(1 row)
-
 SELECT cosine_distance('{1:1,2:2}/2'::sparsevec, '{}/2');
- cosine_distance 
------------------
-             NaN
-(1 row)
-
 SELECT cosine_distance('{1:1,2:1}/2'::sparsevec, '{1:1,2:1}/2');
- cosine_distance 
------------------
-               0
-(1 row)
-
 SELECT cosine_distance('{1:1}/2'::sparsevec, '{2:2}/2');
- cosine_distance 
------------------
-               1
-(1 row)
-
 SELECT cosine_distance('{1:1,2:1}/2'::sparsevec, '{1:-1,2:-1}/2');
- cosine_distance 
------------------
-               2
-(1 row)
-
 SELECT cosine_distance('{1:2}/2'::sparsevec, '{2:2}/2');
- cosine_distance 
------------------
-               1
-(1 row)
-
 SELECT cosine_distance('{2:2}/2'::sparsevec, '{1:2}/2');
- cosine_distance 
------------------
-               1
-(1 row)
-
 SELECT cosine_distance('{1:1,2:2}/2'::sparsevec, '{1:3}/1');
-ERROR:  different sparsevec dimensions 2 and 1
 SELECT cosine_distance('{1:1,2:1}/2'::sparsevec, '{1:1.1,2:1.1}/2');
- cosine_distance 
------------------
-               0
-(1 row)
-
 SELECT cosine_distance('{1:1,2:1}/2'::sparsevec, '{1:-1.1,2:-1.1}/2');
- cosine_distance 
------------------
-               2
-(1 row)
-
 SELECT cosine_distance('{1:3e38}/1'::sparsevec, '{1:3e38}/1');
- cosine_distance 
------------------
-             NaN
-(1 row)
-
 SELECT cosine_distance('{}/1'::sparsevec, '{}/1');
- cosine_distance 
------------------
-             NaN
-(1 row)
-
 SELECT '{1:1,2:2}/2'::sparsevec <=> '{1:2,2:4}/2';
- ?column? 
-----------
-        0
-(1 row)
 
 SELECT l1_distance('{}/2'::sparsevec, '{1:3,2:4}/2');
- l1_distance 
--------------
-           7
-(1 row)
-
 SELECT l1_distance('{}/2'::sparsevec, '{2:1}/2');
- l1_distance 
--------------
-           1
-(1 row)
-
 SELECT l1_distance('{1:1,2:2}/2'::sparsevec, '{1:3}/1');
-ERROR:  different sparsevec dimensions 2 and 1
 SELECT l1_distance('{1:3e38}/1'::sparsevec, '{1:-3e38}/1');
- l1_distance 
--------------
-    Infinity
-(1 row)
-
 SELECT l1_distance('{1:1,3:3,5:5,7:7}/8'::sparsevec, '{2:2,4:4,6:6,8:8}/8');
- l1_distance 
--------------
-          36
-(1 row)
-
 SELECT l1_distance('{1:1,3:3,5:5,7:7,9:9}/9'::sparsevec, '{2:2,4:4,6:6,8:8}/9');
- l1_distance 
--------------
-          45
-(1 row)
-
 SELECT '{}/2'::sparsevec <+> '{1:3,2:4}/2';
- ?column? 
-----------
-        7
-(1 row)
 
 SELECT l2_normalize('{1:3,2:4}/2'::sparsevec);
-  l2_normalize   
------------------
- {1:0.6,2:0.8}/2
-(1 row)
-
 SELECT l2_normalize('{1:3}/2'::sparsevec);
- l2_normalize 
---------------
- {1:1}/2
-(1 row)
-
 SELECT l2_normalize('{2:0.1}/2'::sparsevec);
- l2_normalize 
---------------
- {2:1}/2
-(1 row)
-
 SELECT l2_normalize('{}/2'::sparsevec);
- l2_normalize 
---------------
- {}/2
-(1 row)
-
 SELECT l2_normalize('{1:3e38}/1'::sparsevec);
- l2_normalize 
---------------
- {1:1}/1
-(1 row)
-
 SELECT l2_normalize('{1:3e38,2:1e-37}/2'::sparsevec);
- l2_normalize 
---------------
- {1:1}/2
-(1 row)
-
 SELECT l2_normalize('{2:3e37,4:3e-37,6:4e37,8:4e-37}/9'::sparsevec);
-  l2_normalize   
------------------
- {2:0.6,6:0.8}/9
-(1 row)
-
--- test/sql/vector.sql
+++ test/sql/vector.sql
@@ -1,3 +1,45 @@
+SELECT '[1,2,3]'::vector;
+SELECT '[-1,-2,-3]'::vector;
+SELECT '[1.,2.,3.]'::vector;
+SELECT ' [ 1,  2 ,    3  ] '::vector;
+SELECT '[1.23456]'::vector;
+SELECT '[hello,1]'::vector;
+SELECT '[NaN,1]'::vector;
+SELECT '[Infinity,1]'::vector;
+SELECT '[-Infinity,1]'::vector;
+SELECT '[1.5e38,-1.5e38]'::vector;
+SELECT '[1.5e+38,-1.5e+38]'::vector;
+SELECT '[1.5e-38,-1.5e-38]'::vector;
+SELECT '[4e38,1]'::vector;
+SELECT '[-4e38,1]'::vector;
+SELECT '[1e-46,1]'::vector;
+SELECT '[-1e-46,1]'::vector;
+SELECT '[1,2,3'::vector;
+SELECT '[1,2,3]9'::vector;
+SELECT '1,2,3'::vector;
+SELECT ''::vector;
+SELECT '['::vector;
+SELECT '[ '::vector;
+SELECT '[,'::vector;
+SELECT '[]'::vector;
+SELECT '[ ]'::vector;
+SELECT '[,]'::vector;
+SELECT '[1,]'::vector;
+SELECT '[1a]'::vector;
+SELECT '[1,,3]'::vector;
+SELECT '[1, ,3]'::vector;
+
+SELECT '[1,2,3]'::vector(3);
+SELECT '[1,2,3]'::vector(2);
+SELECT '[1,2,3]'::vector(3, 2);
+SELECT '[1,2,3]'::vector('a');
+SELECT '[1,2,3]'::vector(0);
+SELECT '[1,2,3]'::vector(16001);
+
+SELECT unnest('{"[1,2,3]", "[4,5,6]"}'::vector[]);
+SELECT '{"[1,2,3]"}'::vector(2)[];
+
+
 SELECT '[1,2,3]'::vector + '[4,5,6]';
 SELECT '[3e38]'::vector + '[3e38]';
 SELECT '[1,2]'::vector + '[3]';
