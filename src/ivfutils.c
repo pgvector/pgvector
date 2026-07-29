335dd317cf685705cf82f2074c53d4d41c181cae
Andrew Kane <andrew@ankane.org>
2026-07-28 17:12:03 -0700

    Improved IvfflatCheckMemoryUsage [skip ci]

--- src/ivfutils.c
+++ src/ivfutils.c
@@ -121,7 +121,7 @@ void
 IvfflatCheckMemoryUsage(Size totalSize)
 {
 	/* Add one to error message to ceil */
-	if (totalSize > maintenance_work_mem * (Size) 1024)
+	if (totalSize / 1024 > maintenance_work_mem)
 		ereport(ERROR,
 				(errcode(ERRCODE_PROGRAM_LIMIT_EXCEEDED),
 				 errmsg("memory required is %zu MB, maintenance_work_mem is %d MB",
