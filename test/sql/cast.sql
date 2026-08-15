5e2752dac81998722d29114989daabd6dcd76a0a
Andrew Kane <andrew@ankane.org>
2026-08-15 08:49:45 -0700

    Added more tests for casts [skip ci]

--- test/sql/cast.sql
+++ test/sql/cast.sql
@@ -1,10 +1,10 @@
+-- array to vector
+
 SELECT ARRAY[1,2,3]::vector;
 SELECT ARRAY[1.0,2.0,3.0]::vector;
 SELECT ARRAY[1,2,3]::float4[]::vector;
 SELECT ARRAY[1,2,3]::float8[]::vector;
 SELECT ARRAY[1,2,3]::numeric[]::vector;
-
-SELECT '[1,2,3]'::vector::real[];
 
 SELECT '{1,2,3}'::real[]::vector;
 SELECT '{1,2,3}'::real[]::vector(3);
@@ -22,6 +22,12 @@ SELECT '{1,2,3}'::double precision[]::vector(2);
 SELECT '{4e38,-4e38}'::double precision[]::vector;
 SELECT '{1e-46,-1e-46}'::double precision[]::vector;
 
+-- vector to array
+
+SELECT '[1,2,3]'::vector::real[];
+
+-- vector to halfvec
+
 SELECT '[1,2,3]'::vector::halfvec;
 SELECT '[1,2,3]'::vector::halfvec(3);
 SELECT '[1,2,3]'::vector::halfvec(2);
@@ -31,6 +37,14 @@ SELECT '[1e-8]'::vector::halfvec;
 SELECT '[1,2,3]'::halfvec::vector;
 SELECT '[1,2,3]'::halfvec::vector(3);
 SELECT '[1,2,3]'::halfvec::vector(2);
+
+-- array to halfvec
+
+SELECT ARRAY[1,2,3]::halfvec;
+SELECT ARRAY[1.0,2.0,3.0]::halfvec;
+SELECT ARRAY[1,2,3]::float4[]::halfvec;
+SELECT ARRAY[1,2,3]::float8[]::halfvec;
+SELECT ARRAY[1,2,3]::numeric[]::halfvec;
 
 SELECT '{1,2,3}'::real[]::halfvec;
 SELECT '{1,2,3}'::real[]::halfvec(3);
@@ -44,18 +58,36 @@ SELECT '{{1}}'::real[]::halfvec;
 SELECT '{65520,-65520}'::real[]::halfvec;
 SELECT '{1e-8,-1e-8}'::real[]::halfvec;
 
+SELECT '{1,2,3}'::double precision[]::halfvec;
+SELECT '{1,2,3}'::double precision[]::halfvec(3);
+SELECT '{1,2,3}'::double precision[]::halfvec(2);
+SELECT '{4e38,-4e38}'::double precision[]::halfvec;
+SELECT '{1e-46,-1e-46}'::double precision[]::halfvec;
+
+-- halfvec to array
+
+SELECT '[1,2,3]'::halfvec::real[];
+
+-- vector to sparsevec
+
 SELECT '[0,1.5,0,3.5,0]'::vector::sparsevec;
 SELECT '[0,1.5,0,3.5,0]'::vector::sparsevec(5);
 SELECT '[0,1.5,0,3.5,0]'::vector::sparsevec(4);
+
+-- sparsevec to vector
 
 SELECT '{2:1.5,4:3.5}/5'::sparsevec::vector;
 SELECT '{2:1.5,4:3.5}/5'::sparsevec::vector(5);
 SELECT '{2:1.5,4:3.5}/5'::sparsevec::vector(4);
 SELECT '{}/16001'::sparsevec::vector;
 
+-- halfvec to sparsevec
+
 SELECT '[0,1.5,0,3.5,0]'::halfvec::sparsevec;
 SELECT '[0,1.5,0,3.5,0]'::halfvec::sparsevec(5);
 SELECT '[0,1.5,0,3.5,0]'::halfvec::sparsevec(4);
+
+-- sparsevec to halfvec
 
 SELECT '{2:1.5,4:3.5}/5'::sparsevec::halfvec;
 SELECT '{2:1.5,4:3.5}/5'::sparsevec::halfvec(5);
@@ -63,6 +95,8 @@ SELECT '{2:1.5,4:3.5}/5'::sparsevec::halfvec(4);
 SELECT '{}/16001'::sparsevec::halfvec;
 SELECT '{1:65520}/1'::sparsevec::halfvec;
 SELECT '{1:1e-8}/1'::sparsevec::halfvec;
+
+-- array to sparsevec
 
 SELECT ARRAY[1,0,2,0,3,0]::sparsevec;
 SELECT ARRAY[1.0,0.0,2.0,0.0,3.0,0.0]::sparsevec;
@@ -79,6 +113,8 @@ SELECT '{Infinity}'::real[]::sparsevec;
 SELECT '{-Infinity}'::real[]::sparsevec;
 SELECT '{}'::real[]::sparsevec;
 SELECT '{{1}}'::real[]::sparsevec;
+
+-- max dimensions / elements
 
 SELECT array_agg(n)::vector FROM generate_series(1, 16001) n;
 SELECT array_to_vector(array_agg(n), 16001, false) FROM generate_series(1, 16001) n;
